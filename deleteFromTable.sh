8800dbfc439944ca1568693bba8d934ce122712f
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2020-02-04 22:10:08 +0200

    Adding delete from a table function

---
+++ deleteFromTable.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+function deleteFromTable(){
+    echo "Enter Table name you eant to delete from."
+    read tableName
+    if test -f ~/dbms/$dbname/$tableName
+        then
+            linesInFile=$(cat ~/dbms/$dbname/$tableName | wc -l )
+            if test $linesInFile -gt 0
+                then
+                    echo "Total number of rows: $linesInFile."
+                    echo "Enter line number you want to delete"
+                    read lineNumber
+                    sed -i "${lineNumber}d" ~/dbms/$dbname/$tableName
+                else
+                    echo "Table is empty"
+                fi
+        else
+            echo "Table deos not exist."
+    fi
+
+}
+
+deleteFromTable
