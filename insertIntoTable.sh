d3783c27f9eee8154895f8cb2cd2fe234931aa9c
Mohamed-Zkaria <mohamed.zakaria_10_7@yahoo.com>
2020-02-04 18:18:57 +0200

    Updating insert into table function.

--- insertIntoTable.sh
+++ insertIntoTable.sh
@@ -1,10 +1,59 @@
 #! /bin/bash
 
 function insertIntoTable(){
-    data=$1
-    table=$2
-    sed -i "a $data" $table
+    echo "Enter table name to insert into."
+    read tableName
+    metaTable="."
+    row=""
+    if test -f ~/dbms/$dbname/$tableName 
+        then
+            metaTable="$metaTable$tableName"
+            cols=$(awk -F: '{print $1}' ~/dbms/$dbname/$metaTable)
+            data=$(awk -F: '{print $2}' ~/dbms/$dbname/$metaTable)
+            echo $cols
+            numberOfColumns=$(cat ~/dbms/$dbname/$metaTable | wc -l)
+            currentColumn=1
+            echo "Enter your input for the columns respectivly"
+            for type in $data
+                do
+                    case $type in 
+                    int)
+                        echo "Enter a number value"
+                        read input 
+                        while ! [[ $input =~ ^[0-9]+$ ]]
+                            do
+                                echo "please enter a number value"
+                                read input
+                            done
+                        ;;
+                    string)
+                        echo "Enter a string"
+                        read input 
+                        while ! [[ $input =~ ^[a-Z]+$ ]]
+                            do
+                                echo "please enter a string value"
+                                read input
+                            done
+                        ;;
+                    *)
+                        echo "-default case-"
+                    ;;
+                esac
+                if test $currentColumn -eq $numberOfColumns
+                    then
+                    row="$row$input"
+                else
+                    row="$row$input:"   
+                fi
+                currentColumn=$(($currentColumn + 1))
+
+            done
+            sed -i "a $row" ~/dbms/$dbname/$tableName
+        else 
+            echo "Table does not exist"
+    fi
+
 }
 
 
-insertIntoTable $1 $2
+insertIntoTable 
